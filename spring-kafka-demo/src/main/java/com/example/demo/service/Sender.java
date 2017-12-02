13e53d29dca43e2cf5dea97fae1888343241b57b
heathwind <34190573+heathwind@users.noreply.github.com>
2017-12-02 19:17:38 +0300

    Update Sender.java
    Delete sending hotel to kafka

--- spring-kafka-demo/src/main/java/com/example/demo/service/Sender.java
+++ spring-kafka-demo/src/main/java/com/example/demo/service/Sender.java
@@ -22,9 +22,4 @@ public class Sender {
         System.out.println("Sending " + review.toString());
         kafkaTemplate.send(topic, review.toString());
     }
-    
-    public void sendHotel(Hotel hotel){
-        System.out.println("Sending " + hotel.toString());
-        kafkaTemplate.send(topic, hotel.toString());
-    }
 }
