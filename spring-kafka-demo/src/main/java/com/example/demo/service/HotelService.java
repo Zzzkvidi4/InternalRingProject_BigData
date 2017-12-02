31d2062d2bebbea0b416a4b9bfb5a5f57698b5eb
heathwind <34190573+heathwind@users.noreply.github.com>
2017-12-03 01:09:53 +0300

    Added saving hotel to mongo

--- spring-kafka-demo/src/main/java/com/example/demo/service/HotelService.java
+++ spring-kafka-demo/src/main/java/com/example/demo/service/HotelService.java
@@ -1,13 +1,28 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Hotel;
+import com.mongodb.*;
 import org.springframework.stereotype.Service;
+
+import java.net.UnknownHostException;
 
 @Service
 public class HotelService {
 
     public void save(Hotel hotel){
-        //Here will be adding hotel to Mongo
+        MongoClient mongoClient= null;
+        try {
+            mongoClient = new MongoClient();
+            DB database=mongoClient.getDB("BigData");
+            DBObject hotelObject=new BasicDBObject("id",hotel.getId()).append("name",hotel.getName())
+                    .append("country",hotel.getCountry()).append("city",hotel.getCity())
+                    .append("description",hotel.getDescription()).append("average_score",hotel.getAverageScore())
+                    .append("link",hotel.getLink());
+            DBCollection collection=database.getCollection("Hotels");
+            collection.insert(hotelObject);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }        
     }
 
 }
